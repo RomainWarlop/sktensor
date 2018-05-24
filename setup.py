f90536fcd6f7169387297e4bc3cf487cee227c20
Romain <romain@fifty-five.com>
2018-05-24 18:31:28 +0200

    full commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='sktensor',
+      version='0.0.1',
+      #url='https://github.com/RomainWarlop/RecommenderSystem',
+      author='Maximilian Nickel',
+      license='',
+      packages=['sktensor'],
+      zip_safe=False,
+      include_package_data=True)
